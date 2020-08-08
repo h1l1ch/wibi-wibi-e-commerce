286c891fa76d43975e0641175164443759af102a
Philip Chislou <phliplviii@gmail.com>
2020-08-08 21:23:11 +0800

    Created Man Page and added fixes to App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 	return (
     <div className="App">
       <Switch>
+        {/* <Route path="/checkout"/>
         <Route path="/checkout"/>
         <Route path="/checkout"/>
-        <Route path="/checkout"/>
-        <Route path="/checkout"/>
-        <Route path="/checkout"/>
+        <Route path="/checkout"/> */}
+        <Route path="/" exact component={MainPage}/>
       </Switch>
     </div>
   );
